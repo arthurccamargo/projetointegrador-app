c6c48f6289daff1b6ffb4ffe378b3b87efe2e2f8
Arthur <arthurcalazans@gmail.com>
2025-08-28 21:33:21 -0300

    feat: automacao layout dinamico

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import routes from './routes';
 import PrivateRoute from './routes/PrivateRoute';
+import AppLayout from './layout/AppLayout';
 
 const userRole: string = 'guest';
 
@@ -14,7 +15,11 @@ function App() {
             path={route.path}
             element={
               <PrivateRoute
-                element={route.element}
+                element={
+                  <AppLayout config={route.settings?.layout?.config}>
+                    {route.element}
+                  </AppLayout>
+                }
                 allowedRoles={route.auth}
                 userRole={userRole}
               />
---
+++ src/app/layout/AppLayout.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Navbar from './components/Navbar';
+import type { LayoutConfig } from '../routes/types';
+
+
+type AppLayoutProps = {
+  config?: LayoutConfig;
+  children: React.ReactNode;
+};
+
+const drawerWidth = 220;
+
+const AppLayout: React.FC<AppLayoutProps> = ({ config, children }) => (
+  <Box
+    sx={{
+      display: 'flex',
+      height: '100vh',
+      width: '100vw',
+      overflow: 'hidden',
+    }}
+  >
+    {config?.navbar && (
+      <Navbar drawerWidth={drawerWidth} />
+    )}
+    <Box
+      component="div"
+      sx={{
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+        width: '100%',
+        p: 0,
+        m: 0,
+        overflow: 'hidden',
+      }}
+    >
+      {/* {config?.toolbar && <Toolbar />} */}
+      <Box
+        component="main"
+        sx={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          p: 0,
+          m: 0,
+          height: '100%',
+          width: '100%',
+          overflow: 'auto',
+        }}
+      >
+        {children}
+      </Box>
+      {/* {config?.footer && <Footer />} */}
+    </Box>
+  </Box>
+);
+
+export default AppLayout;
