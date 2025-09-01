7d0272ba4dc8988e7656dc546c30cdd4609b474b
Arthur <arthurcalazans@gmail.com>
2025-08-31 23:44:59 -0300

    feat: type Volunteer

---
+++ src/app/types/Volunteer.ts
@@ -0,0 +1,16 @@
+export interface Volunteer {
+  fullName: string;
+  cpf: string;
+  birthDate?: string;
+  email: string;
+  password: string;
+  phone?: string;
+  cep?: string;
+  street?: string;
+  number?: string;
+  complement?: string;
+  neighborhood?: string;
+  city?: string;
+  state?: string;
+  experiences?: string;
+}
