212e58e0a4bd86b85579bd229dafc51f2d06bac7
Arthur <arthurcalazans@gmail.com>
2025-10-16 18:59:01 -0300

    fix: build android/ run android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.helphub.app',
   appName: 'HelpHub',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    cleartext: true,
+    androidScheme: 'http'
+  }
 };
+
 
 export default config;
