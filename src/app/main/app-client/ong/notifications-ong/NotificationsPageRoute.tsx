ca5d3aeb651981f8f1570acbfbed993792fdfb2a
Arthur <arthurcalazans@gmail.com>
2025-11-08 18:37:01 -0300

    feat: notifications ong page and route

---
+++ src/app/main/app-client/ong/notifications-ong/NotificationsPageRoute.tsx
@@ -0,0 +1,14 @@
+import authRoles from '../../../../auth/authRoles';
+import type { AppRoute } from '../../../../routes/types';
+import NotificationsOngPage from './NotificationsOngPage';
+
+const NotificationsPageRoute: AppRoute = {
+  path: '/notifications-ong',
+  element: <NotificationsOngPage />,
+  settings: {
+    layout: { config: { navbar: true, toolbar: false, footer: false } },
+  },
+  auth: authRoles.user, // VOLUNTEER ou ONG
+};
+
+export default NotificationsPageRoute;
